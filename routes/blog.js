e5fae9495c2a8b4dfb7cd7b174f14a7a7670cedd
Ankitsgit <ankit77us@gmail.com>
2025-06-28 17:40:49 +0530

    blog application commited

--- routes/blog.js
+++ routes/blog.js
@@ -2,7 +2,8 @@ const {Router} = require('express')
 const multer = require('multer')
 const path = require('path')
 const router = Router();
-const Blog = require('../models/blog')
+const Blog = require('../models/blog');
+const Comment = require('../models/comments');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -32,6 +33,26 @@ router.post('/', upload.single('coverImage'), async(req, res)=>{
     return res.redirect(`/blog/${blog._id}`)
 })
 
+router.get('/:id',async(req, res)=>{
+    const blog = await Blog.findById(req.params.id).populate("createdBy");
+    const comments = await Comment.find({blogId: req.params.id}).populate("createdBy")
+    console.log("blog: ",blog)
+    return res.render('blog',{
+        user: req.user,
+        blog,
+        comments,
+    })
+})
+
+router.post('/comment/:blogId',async(req,res)=>{
+    await Comment.create({
+        content: req.body.content,
+        blogId: req.params.blogId,
+        createdBy: req.user._id,
+    })
+    return res.redirect(`/blog/${req.params.blogId}`)
+    
+})
 module.exports = router;
 
 
